384da29524357aedd4cb8efdbf1adc53264e8a3c
Nick Birnberg <nbirnberg@gmail.com>
2015-03-23 13:10:03 +0100

    Complete refactor using new data structures.

--- TreeInduction.py
+++ TreeInduction.py
@@ -1,193 +1,279 @@
-from collections import namedtuple
-import re
-import json
 import math
 
 
-class DataRow:
-    def __init__(self, attribs):
-        self.attribs = attribs
-        self.tuple = namedtuple('Row', attribs)
+class ARFFReader:
+    def __init__(self, file_name):
+        self._read_file(open(file_name, 'r'))
 
-    def parse(self, row):
-        values = []
-        for val in zip(self.attribs, row.split(',')):
-            values.append(val[1])
-        return self.tuple(*values)
+    def _read_file(self, lines_iterator):
+        """
+        This will return the file
+        :param lines_iterator:
+        """
+        attributes = {}
+        attribute_names = []
+        # Parse each attribute into a Attribute => Values dictionary.
+        for line in lines_iterator:
+            # ARFF Comments
+            if line.startswith('%'):
+                continue
 
+            # ARFF Attributes
+            if line.lower().startswith('@attribute'):
+                space_separated = line.split(' ', 2)
+                name = space_separated[1].strip()
+                attribute_names.append(name)
+                fields = space_separated[2].strip('{}\n\r ')
 
-class Tree:
-    def __init__(self, attribs, examples, is_leaf):
-        self.attribs = attribs
+                fields = [field.strip(' \n\r"') for field in fields.split(',')]
+
+                attributes[name] = fields
+
+            # Break so that we can parse data.
+            if line.lower().startswith('@data'):
+                break
+        self.attributes = attributes
+        # Store data in list of Attribute => Value dictionaries.
+        examples = []
+        for line in lines_iterator:
+            # ARFF Comments
+            if line.startswith('%'):
+                continue
+            data_dict = {}
+            for index, value in enumerate(line.split(',')):
+                data_dict[attribute_names[index]] = value.strip()
+            examples.append(data_dict)
         self.examples = examples
-        self.is_leaf = is_leaf
 
 
-def load():
-    with open('testData.arff') as f:
-        whole_text = f.read()
-        split_data = whole_text.split('@DATA')
-        attr_vals = re.findall(r'\{(.+?)\}', split_data[0])
-        tree_vals = []
-        for values in attr_vals:
-            final_vals = values.split(',')
-            tree_vals.append(final_vals)
-        attributes = re.findall(r"'(\w+)'", split_data[0])
+class DecisionNode:
+    """
+    Holds an attribute, as well as the number of positive
+    and negative examples at the node.
+    Has other DecisionNodes or DecisionResults as children
+    for each value of the attribute.
+    """
+    def __init__(self, attribute, num_pos, num_neg):
+        self.branches = {}
+        self.attribute = attribute
+        self.num_pos = num_pos
+        self.num_neg = num_neg
 
-        csv_data = split_data[1].splitlines()
-        csv_data.pop(0)
-        data_list = []
-        for row in csv_data:
-            data_tuple = DataRow(attributes)
-            typed_row = data_tuple.parse(row)
-            data_list.append(typed_row)
-        diction = dict(zip(attributes, tree_vals))
-        decision_tree = Tree(diction, data_list, 0)
-        return decision_tree
+    def add_branch(self, value, subtree):
+        self.branches[value] = subtree
+
+    def display(self, indent=0):
+        print self.attribute + '?'
+        for (value, subtree) in self.branches.iteritems():
+            print ' ' * 4 * indent, value, '==>',
+            subtree.display(indent + 1), '\n'
+
+    def has_only_leafs(self):
+        for subtree in self.branches.itervalues():
+            if isinstance(subtree, DecisionNode):
+                return False
+        return True
 
 
-def highest_count(decision_tree):
-    count = 0
-    for ex in decision_tree.examples:
-        if ex.Wait == '"Yes"':
-            count += 1
-        else:
-            count -= 1
-    if count > 0:
-        return "Wait: Yes"
+class DecisionResult:
+    """
+    Holds a decision (yes/no), as well as the number of positive
+    and negative examples that went into the decision.
+    """
+
+    def __init__(self, (result, num_pos, num_neg)):
+        self.result = result
+        self.num_pos = num_pos
+        self.num_neg = num_neg
+
+    # noinspection PyUnusedLocal
+    def display(self, indent=None):
+        print "Result = " + self.result
+
+
+def decision_tree_learning(examples, attributes, parent_examples):
+    example_classes = [example[DECISION_ATTRIBUTE] for example in examples]
+    # if examples is empty then return PLURALITY-VALUE(parent examples)
+    if not examples:
+        return DecisionResult(plurality_value(parent_examples))
+    # else if all examples have the same classification then return the classification
+    elif len(set(example_classes)) == 1:
+        return DecisionResult(plurality_value(examples))
+    # else if attributes is empty then return PLURALITY-VALUE(examples)
+    elif not attributes:
+        return DecisionResult(plurality_value(examples))
     else:
-        return "Wait: No"
+        # A <- argmax in attributes IMPORTANCE(a, examples)
+        best_attr, num_pos, num_neg = find_best_attr(attributes, examples)
+        # tree <- a new decision tree with root test A
+        tree = DecisionNode(best_attr, num_pos, num_neg)
+        # for each value vk of A do
+        for value in attributes[best_attr]:
+            # exs <- {e : e in examples and e.A = vk}
+            new_examples = []
+            for example in examples:
+                if example[best_attr] == value:
+                    new_examples.append(example)
+            new_attributes = dict(attributes)
+            del new_attributes[best_attr]
+            subtree = decision_tree_learning(new_examples, new_attributes, examples)
+            tree.add_branch(value, subtree)
+        return tree
 
 
-def find_best_attr(passed_tree):
-    decision_tree = passed_tree
+def find_best_attr(attributes, examples):
+    """
+    pick the attribute that goes as far as possible toward providing an exact classification of the examples
+    :param attributes: List of attributes to check.
+    :param examples: List of examples in data set.
+    :return: best attribute
+    """
     highest_gain = 0
-    best_attribute = 0
-    num_attributes = len(decision_tree.attribs) - 1
+    best_num_pos = 0
+    best_num_neg = 0
+    best_attribute = None
 
-    for attrib in range(num_attributes):
-        gain_val = 1 - gain(decision_tree, decision_tree.attribs.keys()[attrib])
+    for attribute, values in attributes.iteritems():
+        gain_val, num_pos, num_neg = gain(attribute, values, examples)
         if gain_val >= highest_gain:
-            best_attribute = decision_tree.attribs.keys()[attrib]
+            best_attribute = attribute
             highest_gain = gain_val
-    return best_attribute
+            best_num_pos = num_pos
+            best_num_neg = num_neg
+    return best_attribute, best_num_pos, best_num_neg
 
 
-def gain(passed_tree, test_attribute):
-    decision_tree = passed_tree
-    attrib = test_attribute
+def gain(attribute, values, examples):
+    """
+    Calculates gain.
+    :param attribute: Attribute to calculate gain on.
+    :param values: All possible values of the attribute.
+    :param examples: List of examples in data set.
+    :return: Gain of attribute.
+    """
     total_gain = float(0)
-    for val in decision_tree.attribs[attrib]:
-        new_data = []
+    num_pos = 0
+    num_neg = 0
+    for value in values:
         positive = 0
         negative = 0
-        for ex in decision_tree.examples:
-            if getattr(ex, attrib) == val:
-                new_data.append(ex)
-                if getattr(ex, "Wait") == '"Yes"':
+        for example in examples:
+            if example[attribute] == value:
+                if example[DECISION_ATTRIBUTE] == DECISION_VALUES[0]:
                     positive += 1
                 else:
                     negative += 1
         if negative + positive > 0:
             qval = float(positive) / (negative + positive)
             ent = entropy(qval)
-
         else:
             ent = 0
-        prob = float(len(new_data)) / len(decision_tree.examples)
-        total_gain += float((ent * prob))
-    return total_gain
+        prob = float(positive + negative) / len(examples)
+        total_gain += float(ent * prob)
+        num_pos += positive
+        num_neg += negative
+    return 1 - total_gain, num_pos, num_neg
 
 
-def entropy(qval):
-    if qval == 0:
-        return 0
-    if qval == 1:
-        return 0
-    log1 = math.log(qval, 2)
-    log2 = math.log(1 - qval, 2)
-    ent = -(qval * log1 + (1 - qval) * log2)
-    return ent
-
-
-def chisquared(passed_tree, test_attrib, temp_attribs):
-    decision_tree = passed_tree
-    attrib = test_attrib
-    attrib_data = temp_attribs
+def plurality_value(examples):
+    """
+    :param examples: Current set of examples passed in.
+    :return: Tuple containing decision, and number of
+            positive/negative examples
+            Decision is random if there's an equal amount.
+    """
     num_pos = 0
     num_neg = 0
-    delta = 0
-    compare = 0
-    print decision_tree.examples
-    print attrib
-    df = len(attrib_data[attrib]) - 1
-    if df == 1:
-        compare = 2.706
-    if df == 2:
-        compare = 4.605
-    if df == 3:
-        compare = 6.251
-    for text_ex in decision_tree.examples:
-        if text_ex.Wait == '"Yes"':
+    for example in examples:
+        if example[DECISION_ATTRIBUTE] == DECISION_VALUES[0]:
             num_pos += 1
         else:
             num_neg += 1
-    for val in attrib_data[attrib]:
-        new_data = []
-        positive = 0
-        negative = 0
-        for ex in decision_tree.examples:
-            if getattr(ex, attrib) == val:
-                new_data.append(ex)
-                if getattr(ex, "Wait") == '"Yes"':
-                    positive += 1
-                else:
-                    negative += 1
-        pos_hat = num_pos * ((positive + negative) / float(num_pos + num_neg))
-        neg_hat = num_neg * ((positive + negative) / float(num_pos + num_neg))
-        delta += (math.pow(positive - pos_hat, 2) / pos_hat) + (math.pow(negative - neg_hat, 2) / neg_hat)
-        print 'delta=', delta
-    print delta
-    print compare
+    if num_pos > num_neg:
+        return "Yes", num_pos, num_neg
+    elif num_pos < num_neg:
+        return "No", num_pos, num_neg
+    else:
+        import random
+        random_int = random.randint(1, 2)
+        if random_int == 1:
+            return "Yes", num_pos, num_neg
+        else:
+            return "No", num_pos, num_neg
+
+
+def entropy(q_val):
+    if q_val == 0:
+        return 0
+    if q_val == 1:
+        return 0
+    log1 = math.log(q_val, 2)
+    log2 = math.log(1 - q_val, 2)
+    ent = -(q_val * log1 + (1 - q_val) * log2)
+    return ent
+
+
+def prune(d_tree, attributes):
+    """
+    Recursively prunes tree from bottom up.... after going top->down
+    :param d_tree: node to be pruned
+    :param attributes: List of attributes in dataset. Only needed to calculate
+                       degrees of freedom in chi-squared test.
+    :return: pruned tree
+    """
+    if d_tree.has_only_leafs():
+        if reject_null_hypothesis(d_tree, attributes):
+            if d_tree.num_pos > d_tree.num_neg:
+                return DecisionResult(("Yes", d_tree.num_pos, d_tree.num_neg))
+            else:
+                return DecisionResult(("No", d_tree.num_pos, d_tree.num_neg))
+        else:
+            return d_tree
+    else:
+        # Recurse onto children DecisionNodes
+        for label, subtree in d_tree.branches.iteritems():
+            if isinstance(subtree, DecisionNode):
+                d_tree.branches[label] = prune(subtree, attributes)
+    return d_tree
+
+
+def reject_null_hypothesis(tree_node, attributes):
+    from scipy.stats import chi2
+
+    total_pos = tree_node.num_pos
+    total_neg = tree_node.num_neg
+    # Calculate the value that Delta would reject at 5% level
+    df = len(attributes[tree_node.attribute]) - 1
+    compare = chi2.ppf(.95, df)
+    delta = 0
+    for decision in tree_node.branches.itervalues():
+        value_pos = decision.num_pos
+        value_neg = decision.num_neg
+        pos_hat = total_pos * ((value_pos + value_neg) / float(total_pos + total_neg))
+        neg_hat = total_neg * ((value_pos + value_neg) / float(total_pos + total_neg))
+        delta += (math.pow(value_pos - pos_hat, 2) / pos_hat) + (math.pow(value_neg - neg_hat, 2) / neg_hat)
     return delta >= compare
 
 
-def induction(passed_tree):
-    decision_tree = passed_tree
+if __name__ == '__main__':
+    # Read ARFF file. Current reader only supports attributes with discrete domains.
+    read_arff = ARFFReader('testData.arff')
 
-    class_check = [x[-1] for x in decision_tree.examples]
-    if len(class_check) - 1 <= 0:
-        passed_tree.isLeaf = 1
-        return highest_count(decision_tree)
-    elif class_check.count(class_check[0]) == len(class_check):
-        passed_tree.isLeaf = 1
-        return class_check[0]
-    else:
-        passed_tree.isLeaf = 0
-        subtrees = []
-        best_attr = find_best_attr(decision_tree)
-        temp_attribs = decision_tree.attribs.copy()
-        del (decision_tree.attribs[best_attr])
-        return_tree = {best_attr: {}}
-        for val in temp_attribs[best_attr]:
-            new_data = []
-            for ex in decision_tree.examples:
-                if getattr(ex, best_attr) == val:
-                    new_data.append(ex)
+    # Choose attribute to form decision on.
+    DECISION_ATTRIBUTE = 'will_wait'
+    DECISION_VALUES = read_arff.attributes[DECISION_ATTRIBUTE]
 
-            temp_tree = Tree(decision_tree.attribs, new_data, 0)
-            subtrees.append(temp_tree)
-            return_tree[best_attr][val] = induction(temp_tree)
-        if all([sub.isLeaf == 1 for sub in subtrees]):
-            print
-            if not chisquared(decision_tree, best_attr, temp_attribs):
-                print 'alsoran'
-                return {best_attr: highest_count(decision_tree)}
-        return return_tree
+    del read_arff.attributes[DECISION_ATTRIBUTE]
 
+    # Create decision tree.
+    decision_tree = decision_tree_learning(read_arff.examples, read_arff.attributes, None)
+    decision_tree.display()
 
-tree = load()
-
-induced = induction(tree)
-
-print json.dumps(induced, indent=4)
+    # Prune decision tree:
+    pruned_tree = decision_tree
+    number_of_prune_iterations = 1
+    for i in range(0, number_of_prune_iterations):
+        pruned_tree = prune(pruned_tree, read_arff.attributes)
+    # Print pruned tree
+    print '\n\n#######\nPruned Tree:\n#######\n\n'
+    pruned_tree.display()
